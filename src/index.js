7d0064e8a6d2a0e559697284a66e0d66227edfee
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-07 19:42:08 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
-
-//* can also store process.env.PORT in any variable and use *//
 
 connectDB()
   .then(() => {
@@ -17,26 +15,3 @@ connectDB()
   .catch((err) => {
     console.log("MONGO db connection failed !!!", err);
   });
-
-/* APPROACH 1: using try catch for error handling and using async await...
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-        console.log("ERRR: ", error)
-        throw error
-    })
-
-    app.listen(process.env.PORT, () => {
-        console.log(`App is listening on port ${process.env.PORT}`);
-    })
-
-  } catch (error) {
-    console.log("Error: ", error);
-    throw err;
-  }
-})();
-*/
