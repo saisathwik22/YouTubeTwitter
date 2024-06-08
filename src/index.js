32e3eccc2d2f36c9d7829b09617a5294823808e7
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-08 18:54:48 +0530

    port changed from 8000 to 8001

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8001, () => {
       console.log(`Server is running at port: ${process.env.PORT}`);
     });
   })
-  .catch((err) => {
-    console.log("MONGO db connection failed !!!", err);
+  .catch((error) => {
+    console.log("MONGO db connection failed !!!", error);
   });
