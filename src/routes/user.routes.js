103edff4a8758dcfc4b4a26752f0bc79c3cd8662
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-19 14:36:00 +0530

    changes in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -31,24 +31,28 @@ router.route("/register").post(
   registerUser
 );
 
-router.route("/login").post(loginUser);
+router.route("/login").post(upload.none(), loginUser);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router
+  .route("/change-password")
+  .post(upload.none(), verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
-router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/update-user")
+  .patch(upload.none(), verifyJWT, updateAccountDetails);
 
 router
-  .route("/avatar")
+  .route("/update-avatar")
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
-  .route("/cover-image")
+  .route("/update-coverImg")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
-router.route("/history").get(verifyJWT, getWatchHistory);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
