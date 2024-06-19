b801a037eb8a6118fc69b0986907beae9a5565d7
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-19 15:00:51 +0530

    Adding routes for all controllers

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import {
+  createTweet,
+  deleteTweet,
+  updateTweet,
+  getUserTweets,
+} from "../controllers/tweet.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = Router();
+
+router.use(verifyJWT, upload.none());
+
+router.route("/").post(createTweet);
+router.route("/user/:userId").get(getUserTweets);
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+
+export default router;
